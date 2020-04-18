310f4c9c990eb43552406494d99087b3704b9943
Cara <Cara@LAPTOP-C0EO5GIK>
2020-04-18 15:50:44 +0100

    added Pair functionality, track now outputs 2 variables: output and EnergyLossArray

---
+++ src/protontherapy/RBE.java
@@ -0,0 +1,82 @@
+package protontherapy;
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.stream.*;
+
+public class RBE {
+    
+public double [] getPhantomStart(double [] phantomPosition) {
+       double [] start = Arrays.copyOfRange(phantomPosition, 0, 2); 
+       return start;
+}
+
+public double [] getPhantomEnd(double [] phantomPosition) {
+        double [] end = Arrays.copyOfRange(phantomPosition, 3, 5);
+       return end;
+}
+    
+public double [][][] LET(int nbins, int numberOfEvents, double [] phantomPosition) {
+    // intialising array to store LET of each voxel
+    double [][][] LET = new double [nbins][nbins][nbins];
+    
+    double [] start = getPhantomStart(phantomPosition);
+    double [] end = getPhantomEnd(phantomPosition);
+    
+    // initialise voxel coordinates
+    Voxel voxel_x = new Voxel(nbins, start, end, "RBE", false);
+    voxel_x.getVoxelx(phantomPosition, nbins);
+    
+    Voxel voxel_y = new Voxel(nbins, start, end, "RBE", false);
+    voxel_y.getVoxely(phantomPosition, nbins);
+    
+    Voxel voxel_z = new Voxel(nbins, start, end, "RBE", false);
+    voxel_z.getVoxelz(phantomPosition, nbins);
+        
+    // for all particles
+    for (int a = 0; a < numberOfEvents; a++) {
+        
+        // for voxels in z
+        for (int i = 0; i < nbins; i++){
+            // for voxels in x
+            for (int j = 0; j < nbins; j++) {
+                // for voxels in y
+                for (int k = 0; k < nbins; k++) {
+                    
+                    // checking if particle is in certain voxel volume
+                    if (x > ) {
+                        
+                    }
+                
+
+    
+    /*
+    for all voxels
+        for all particles
+            if particle is in particular voxel
+                calculate dE
+                store in array
+    
+                calculate dz
+                store in array
+    
+    
+        calculate LET(v) for each voxel
+        store in array
+    
+    */
+    
+    
+                            }    
+}
+}
+        return LET;
+}
+}
+
+
+
+
+    
+
+
