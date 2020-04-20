1ea54e295669454d7da829676da1052fd6d766f0
Cara <Cara@LAPTOP-C0EO5GIK>
2020-04-20 16:34:22 +0100

    added simpleRBE histogram method

--- src/protontherapy/RBE.java
+++ src/protontherapy/RBE.java
@@ -149,13 +149,14 @@ public class RBE extends Parameters {
         double dist = findTrackLength(distance);
         double dE_dz = totalEloss/dist;
         double LETParticle = (totalEloss*dE_dz*(1/rho))/(totalEloss);
+
     
         for(int zi = 0; zi<nbins; zi++){
             for(int xi = 0; xi<nbins;xi++){
                 for(int yi = 0; yi<nbins;yi++){
                     // if particle is in tumour volume, fill LET hist
                     if(isInSphere(xi,yi,zi)){
-                        LET.fill(LETParticle);                      
+                        LET.fill(LETParticle);
                     }
                 }
             }
@@ -194,23 +195,35 @@ public class RBE extends Parameters {
     
 // CASE 1 - RBE = 1.1
     
-    public double [][][] simpleRBE(double RBE, double [][][] dE, int nbins) {
-       
-        // initialising RBE weighted dose for simple case
-        double [][][] simpleRBEWeight = new double [nbins][nbins][nbins];
+    // returns RBE weighted dose
+    public double getSimpleRBEweight(double totalEloss) {
+                double RBE = 1.1;
+                double simpleRBEweight = RBE*totalEloss;      
+        return simpleRBEweight;
+    }
+    
+    // returns RBE weighted dose histogram
+    public Histogram [] simpleRBEHist(int ke, int nbin, double [] EnergyLossArray) {
+        Histogram simpleRBE = new Histogram(DVHnbins, 0, 150, "Simple RBE");
         
-        // for voxels in z
-        for (int i = 0; i < nbins; i++){
-            // for voxels in x
-            for (int j = 0; j < nbins; j++) {
-                // for voxels in y
-                for (int k = 0; k < nbins; k++) {
-                    simpleRBEWeight[i][j][k] = RBE*dE[i][j][k];
+        double totalEloss = finddE(EnergyLossArray);
+        double simpleRBEweight = getSimpleRBEweight(totalEloss);
+        
+        for(int zi = 0; zi<nbins; zi++){
+            for(int xi = 0; xi<nbins;xi++){
+                for(int yi = 0; yi<nbins;yi++){
+                    // if particle is in tumour volume, fill LET hist
+                    if(isInSphere(xi,yi,zi)){
+                        simpleRBE.fill(simpleRBEweight);
+                    }
                 }
             }
         }
+        Histogram[] hists = {simpleRBE};        
+        return hists;
         
-        return simpleRBEWeight;
+
+        
     }
     
 // CURRENTLY ALL METHODS BELOW ARE VOID - NEED TO WORK OUT HOW TO INCLUDE FILL FUNCTION
@@ -296,18 +309,7 @@ public class RBE extends Parameters {
         
         }
         
-    
-    public double [] getPhantomStart(double [] phantomPosition) {
-           double [] start = Arrays.copyOfRange(phantomPosition, 0, 2); 
-           return start;
-    }
-
-    public double [] getPhantomEnd(double [] phantomPosition) {
-            double [] end = Arrays.copyOfRange(phantomPosition, 3, 5);
-           return end;
-    }
-
-    // returns energy lost by single particle in said voxel
+            // returns energy lost by single particle in said voxel
     public double finddE (double [] EnergyLossArray) {
         double totalEloss = 0;
         // summing all elements in array
@@ -325,53 +327,68 @@ public class RBE extends Parameters {
             totalTrackLength += stepsize[i];
         }
         return totalTrackLength;  
-    }    
-
-        // returns voxel x coordinates
-    public double [] getVoxelx(double [] phantomPosition, int nbins){
-        // initialise voxel x array
-        double [] voxelx = new double [nbins+1];
-        // returns start and end x values
-        double startx = phantomPosition[0];
-        double endx = phantomPosition[3];
-        // calculates step length in x
-        double stepdist = (phantomPosition[3] - phantomPosition[0])/nbins; 
-        // fills voxelx array with x coords
-        for (int i = 0; i < nbins; i++){
-            voxelx[i] = startx + i*stepdist;
-        }
-        return voxelx;
-    }
-    
-    // returns voxel y coordinates
-    public double [] getVoxely(double [] phantomPosition, int nbins){
-        // initialise voxel y array
-        double [] voxely = new double [nbins+1];
-        // returns start and end y values
-        double starty = phantomPosition[1];
-        double endy = phantomPosition[4];
-        // calculates step length in y
-        double stepdist = (phantomPosition[4] - phantomPosition[1])/nbins; 
-        // fills voxely array with y coords
-        for (int i = 0; i < nbins; i++){
-            voxely[i] = starty + i*stepdist;
-        }
-        return voxely;
-    }
-    
-    // returns voxel z coordinates
-    public double [] getVoxelz(double [] phantomPosition, int nbins){
-        // initialise voxel z array
-        double [] voxelz = new double [nbins+1];
-        // returns start and end z values
-        double startz = phantomPosition[2];
-        double endz = phantomPosition[5];
-        // calculates step length in z
-        double stepdist = (phantomPosition[5] - phantomPosition[2])/nbins; 
-        // fills voxelz array with z coords
-        for (int i = 0; i < nbins; i++){
-            voxelz[i] = startz + i*stepdist;
-        }
-        return voxelz;
-    }
 }
+}
+        
+    
+//    public double [] getPhantomStart(double [] phantomPosition) {
+//           double [] start = Arrays.copyOfRange(phantomPosition, 0, 2); 
+//           return start;
+//    }
+//
+//    public double [] getPhantomEnd(double [] phantomPosition) {
+//            double [] end = Arrays.copyOfRange(phantomPosition, 3, 5);
+//           return end;
+//    }
+//
+
+//    }    
+//
+//        // returns voxel x coordinates
+//    public double [] getVoxelx(double [] phantomPosition, int nbins){
+//        // initialise voxel x array
+//        double [] voxelx = new double [nbins+1];
+//        // returns start and end x values
+//        double startx = phantomPosition[0];
+//        double endx = phantomPosition[3];
+//        // calculates step length in x
+//        double stepdist = (phantomPosition[3] - phantomPosition[0])/nbins; 
+//        // fills voxelx array with x coords
+//        for (int i = 0; i < nbins; i++){
+//            voxelx[i] = startx + i*stepdist;
+//        }
+//        return voxelx;
+//    }
+//    
+//    // returns voxel y coordinates
+//    public double [] getVoxely(double [] phantomPosition, int nbins){
+//        // initialise voxel y array
+//        double [] voxely = new double [nbins+1];
+//        // returns start and end y values
+//        double starty = phantomPosition[1];
+//        double endy = phantomPosition[4];
+//        // calculates step length in y
+//        double stepdist = (phantomPosition[4] - phantomPosition[1])/nbins; 
+//        // fills voxely array with y coords
+//        for (int i = 0; i < nbins; i++){
+//            voxely[i] = starty + i*stepdist;
+//        }
+//        return voxely;
+//    }
+//    
+//    // returns voxel z coordinates
+//    public double [] getVoxelz(double [] phantomPosition, int nbins){
+//        // initialise voxel z array
+//        double [] voxelz = new double [nbins+1];
+//        // returns start and end z values
+//        double startz = phantomPosition[2];
+//        double endz = phantomPosition[5];
+//        // calculates step length in z
+//        double stepdist = (phantomPosition[5] - phantomPosition[2])/nbins; 
+//        // fills voxelz array with z coords
+//        for (int i = 0; i < nbins; i++){
+//            voxelz[i] = startz + i*stepdist;
+//        }
+//        return voxelz;
+//    }
+//}
